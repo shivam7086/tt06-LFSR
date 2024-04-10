9a17a6fa980a763c93c07ef19e7f6252d0a3dba0
beaprog <86886452+beaprog@users.noreply.github.com>
2024-04-10 12:27:42 +0530

    Update LFSR.v

--- src/LFSR.v
+++ src/LFSR.v
@@ -1,14 +1,34 @@
-`timescale 1ns / 1ps
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-module LFSR( out,clk,rst);
-input clk,rst;
-output reg[3:0] out;
-always@(posedge clk)
+`define default_netname none
+
+module tt_um_Johnson_Counter_wmk7fe (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+ 
+ reg[3:0] out;
+ assign rst = rst_n;
+ assign uo_out = out;
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+    always@(posedge clk)
 begin 
 if(rst)
 out=4'b1;
 else
 out={out[2:0],(out[3]^out[2])};
 end
- 
 endmodule
