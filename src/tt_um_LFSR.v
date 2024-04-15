76f4020b255583a72f6c9d48e8e3d40f426db886
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-04-15 09:43:13 +0530

    Update tt_um_LFSR.v

--- src/tt_um_LFSR.v
+++ src/tt_um_LFSR.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_LFSR_shivam (
+module tt_um_Counter_shivam (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
